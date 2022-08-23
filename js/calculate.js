5e3eb4ed5c621d644d453626ef171000ef9f6492
unknown <mejbacsedu@gmail.com>
2022-08-23 13:23:48 +0600

    expenses-section

--- js/calculate.js
+++ js/calculate.js
@@ -2,20 +2,25 @@ const calculatePlayerCost=()=>{
     const playerCostEl = document.getElementById('per-player-cost');
     const playerCost = playerCostEl.value;
     const playerCostToNumber = parseInt(playerCost);
+    const totalNumberOfPlayer =getTotalPlayersNumber();
+    console.log(totalNumberOfPlayer);
     if(playerCostToNumber<0){
         alert('Please Enter a positive Number.');
     }else if(playerCost==''){
         alert('Enter the Per Player Cost');
+    }else if(totalNumberOfPlayer<1){
+        alert('Please Add player first');
     }else{
-
-        const totalNumberOfPlayer =getTotalPlayersNumber();
         const playerExpenses= calculatePlayerExpenses(totalNumberOfPlayer, playerCostToNumber);
         const playerExpensesEl = document.getElementById('player-expenses');
         playerExpensesEl.innerText = playerExpenses;
+        
     }
     
 }
+const calculateTotalCost=()=>{
 
+}
 const calculatePlayerExpenses=(totalNumberOfPlayer, playerCostPerPlayer)=>{
      const totalCostForPlayers = totalNumberOfPlayer*playerCostPerPlayer;
      return totalCostForPlayers; 
