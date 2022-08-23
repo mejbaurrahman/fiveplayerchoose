876da75ba3093063a53c608007e90405908f8ee9
unknown <mejbacsedu@gmail.com>
2022-08-23 13:47:14 +0600

    total expenses calculation

--- js/calculate.js
+++ js/calculate.js
@@ -1,27 +1,52 @@
-const calculatePlayerCost=()=>{
-    const playerCostEl = document.getElementById('per-player-cost');
-    const playerCost = playerCostEl.value;
-    const playerCostToNumber = parseInt(playerCost);
+const calculatePlayerBudget=()=>{
+    const playerBudgetEl = document.getElementById('per-player-cost');
+    const playerBudget = playerBudgetEl.value;
+    const playerBudgetToNumber = parseFloat(playerBudget);
     const totalNumberOfPlayer =getTotalPlayersNumber();
-    console.log(totalNumberOfPlayer);
-    if(playerCostToNumber<0){
+    if(playerBudgetToNumber<0){
         alert('Please Enter a positive Number.');
-    }else if(playerCost==''){
-        alert('Enter the Per Player Cost');
-    }else if(totalNumberOfPlayer<1){
-        alert('Please Add player first');
+    }else if(playerBudget==''){
+        alert('Enter the Per Player Budget');
     }else{
-        const playerExpenses= calculatePlayerExpenses(totalNumberOfPlayer, playerCostToNumber);
+        const playerExpenses= calculatePlayerExpenses(totalNumberOfPlayer, playerBudgetToNumber);
         const playerExpensesEl = document.getElementById('player-expenses');
         playerExpensesEl.innerText = playerExpenses;
         
     }
     
 }
-const calculateTotalCost=()=>{
+const calculateTotalBudget=()=>{
+    const managerBudgetEl = document.getElementById('manager-budget');
+    const coachBudgetEl = document.getElementById('coach-budget');
+    const playerExpensesEl = document.getElementById('player-expenses');
+    const playerExpenses = playerExpensesEl.innerText;
+    const managerBudget = managerBudgetEl.value;
+    const coachBudget = coachBudgetEl.value;
+
+    if(parseFloat(managerBudget)<0){
+        alert('Enter a positive Number for Manager Budget');
+    }else if(parseFloat(coachBudget)<0){
+        alert('Enter a positive Number for Coach Budget');
+    }else{
+        const totalBudget = totalBugetCalculate(playerExpenses, managerBudget, coachBudget);
+        const totalBudgetEl = document.getElementById('total-budget');
+        totalBudgetEl.innerText= totalBudget;
+    }
+    
+
 
 }
-const calculatePlayerExpenses=(totalNumberOfPlayer, playerCostPerPlayer)=>{
-     const totalCostForPlayers = totalNumberOfPlayer*playerCostPerPlayer;
-     return totalCostForPlayers; 
+const calculatePlayerExpenses=(totalNumberOfPlayer, playerBudgetPerPlayer)=>{
+     const totalBudgetForPlayers = totalNumberOfPlayer*playerBudgetPerPlayer;
+     return totalBudgetForPlayers; 
+}
+
+const totalBugetCalculate=(playerExpenses, managerBudget, coachBudget)=>{
+    const playerExpensesToNumber = parseFloat(playerExpenses);
+    const managerBudgetToNumber = parseFloat(managerBudget);
+    const coachBudgetToNumber = parseFloat(coachBudget);
+
+   const totalBudget = playerExpensesToNumber+managerBudgetToNumber+coachBudgetToNumber;
+    return totalBudget;
+   
 }
